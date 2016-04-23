f789eb61832e0214d5b34a5a0945c16a70cd9c5f
pn <pedro.nakibar@xdevel.com.br>
2016-04-23 19:59:32 -0300

    facebook likes

--- app/admin/dashboards/Dashboard1.jsx
+++ app/admin/dashboards/Dashboard1.jsx
@@ -1,77 +1,85 @@
 import React from 'react'
 import {Row, Col} from 'react-flexbox-grid/lib/index'
 import {Dashboard, DashboardCard} from './components/Dashboard'
+import FontIcon from 'material-ui/FontIcon'
+
+import Paper from 'material-ui/Paper'
+import {indigo500, indigo900} from 'material-ui/styles/colors'
 
 export default class Dashboard1 extends React.Component {
-  render () {
-    const tilesData1 = [
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-361390.jpg',
-        title: 'Image 1',
-        author: 'Author 1'
-      },
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-46230.jpg',
-        title: 'Image 2',
-        author: 'Author 2'
-      },
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-350916.jpg',
-        title: 'Image 3',
-        author: 'Author 3'
-      },
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-75602.jpg',
-        title: 'Image 4',
-        author: 'Author 4'
-      },
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-70126.jpg',
-        title: 'Image 5',
-        author: 'Author 5'
-      },
-      {
-        img: 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-21732.jpg',
-        title: 'Image 6',
-        author: 'Author 7'
-      }
-    ]
+  constructor (props) {
+    super(props)
+    const likesCount = 99
 
-    const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-     Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-     Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-     Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.`
-    const rowStyle = {
-      '-webkit-justify-content': 'space-between',
-      'justify-content': 'space-between'
+    this.state = {
+      likesCount: likesCount,
+      likesCountString: this._numberToFormattedString(likesCount),
     }
+  }
+  
+  _numberToFormattedString (number) {
+    if (number < 1000)
+      return number
+    else {
+      return this._numberToFormattedString(Math.floor(number/1000)) + 'k'
+    }
+  }
+
+  render () {
     return (
       <Dashboard title={this.props.title}>
-        <div className='1'>
-          <Row style={rowStyle}>
-            {tilesData1.map((e, i) =>
-              <Col xs={6} sm={3} md={2}>
-                <div key={i}>
-                  <DashboardCard title={e.title} media={<img src={e.img}/>} text={text}/>
-                </div>
-              </Col>
-            )}
-            {tilesData1.map((e, i) =>
-              <Col xs={6} sm={3} md={2}>
-                <div key={i}>
-                  <DashboardCard title={e.title} media={<img src={e.img}/>} text={text}/>
-                </div>
-              </Col>
-            )}
-          </Row>
-        </div>
+        {this._likeIcon()}
       </Dashboard>
     )
   }
+  _likeIcon () {
+    console.log(indigo500)
+    const divStyle = {
+      display: 'inline-block'
+    }
+
+    const style1 = {
+      backgroundColor: indigo500,
+      height: '120px',
+      width: '120px',
+      margin: -1,
+      display: 'inline-block',
+      textAlign: 'center',
+      lineHeight: '120px'
+    }
+    const style2 = {
+      backgroundColor: indigo900,
+      height: '120px',
+      width: '120px',
+      margin: -1,
+      display: 'inline-block',
+      textAlign: 'center',
+      lineHeight: '120px'
+    }
+    const fontStyle = {
+      color: '#FFFFFF',
+      display: 'inline-block',
+      verticalAlign: 'middle',
+      lineHeight: 'normal',
+      fontSize: '70px'
+    }
+    const iconStyle = {
+        color: '#FFFFFF',
+        fontSize: '110px',
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        lineHeight: 'normal'
+    }
+
+    return (
+      <Row>
+        <Paper rounded={false} style={style1} zDepth={1}>
+          <FontIcon className="material-icons" style={iconStyle}>thumb_up</FontIcon>
+        </Paper>
+        <Paper rounded={false} style={style2} zDepth={1} >
+          <span style={fontStyle}>{this.state.likesCountString}</span>
+        </Paper>
+      </Row>
+    )
+  }
 }
-/*
-Dashboard1.propTypes = {
-  title: React.PropTypes.string,
-  subtitle: React.PropTypes.string
-}
-*/
